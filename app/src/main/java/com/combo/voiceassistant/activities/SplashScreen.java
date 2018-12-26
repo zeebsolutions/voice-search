e03d862f7e475c8bc5be434cc75b0662e61b1bd5
Muhammad <valleyligts@gmail.com>
2018-12-26 12:53:33 +0500

    Initial Commit

--- app/src/main/java/com/combo/voiceassistant/activities/SplashScreen.java
+++ app/src/main/java/com/combo/voiceassistant/activities/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        //Fabric.with(this, new Crashlytics());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);

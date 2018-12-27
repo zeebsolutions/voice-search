8b26b44cbaa2bbdae52cd2e090cf6e47d09a9f34
ziamo <ziamoon11@gmail.com>
2018-12-28 00:54:15 +0500

    version 2.0

--- app/src/main/java/com/tulip/voicesearch/activities/MainActivity.java
+++ app/src/main/java/com/tulip/voicesearch/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tulip.voicesearch.activities;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,7 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tulip.voicesearch.R;
@@ -243,4 +248,155 @@ public class MainActivity extends AppCompatActivity {
             tasks = new VoiceTasks(this);
         }
     }
+    public void rattingDialog()    {
+        final Dialog view = new Dialog(this);
+        view.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        view.setContentView(R.layout.ranking_app_dialog_box);
+        Button Exit=view.findViewById(R.id.Exit);
+        Button Feedback=view.findViewById(R.id.feedback);
+        Button Cancel=view.findViewById(R.id.Cancel);
+       /*
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog view = builder.create();
+        final View view = LayoutInflater.from(this).inflate(R.layout.ranking_app_dialog_box,null);
+        Button Exit=view.findViewById(R.id.Exit);
+        Button Feedback=view.findViewById(R.id.feedback);
+        Button Cancel=view.findViewById(R.id.Cancel);
+*/
+        ImageButton adButton0=view.findViewById(R.id.ad_button0);
+        adButton0.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.tulip.routefinder")));}});
+        ImageButton adButton1=view.findViewById(R.id.ad_button1);
+        adButton1.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.voiceassistant")));}});
+        ImageButton adButton2=view.findViewById(R.id.ad_button2);
+        adButton2.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.navigation")));}});
+        ImageButton adButton3=view.findViewById(R.id.ad_button3);
+        adButton3.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.tourtheworld")));}});
+        ImageButton adButton4=view.findViewById(R.id.ad_button4);
+        adButton4.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sparrow.quiztrivia")));}});
+        ImageButton adButton5=view.findViewById(R.id.ad_button5);
+        adButton5.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.appsstandard.unitconverter")));}});
+        ImageButton adButton6=view.findViewById(R.id.ad_button6);
+        adButton6.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sparrow.fun")));}});
+
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    finishAffinity();
+                }
+                MainActivity.this.finish();
+                System.exit(0);
+            }
+        });
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.cancel();
+            }
+        });        Feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=" + MainActivity.this.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" +
+                                    MainActivity.this.getPackageName())));
+                }
+            }
+        });
+/*
+        builder.setView(view);
+        view.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+*/
+        view.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        rattingDialog();
+    }
+
+
+    private void policyDialog() {
+        final Dialog view = new Dialog(this);
+        view.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        view.setContentView(R.layout.app_policy);
+        Button Exit=view.findViewById(R.id.Exit);
+        Button Feedback=view.findViewById(R.id.feedback);
+        Button Cancel=view.findViewById(R.id.Cancel);
+        TextView appPolicy=view.findViewById(R.id.app_policy);
+        appPolicy.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.freeprivacypolicy.com/privacy/view/525aeb460d6c6e930ea8b4f10ced4718")));}});
+
+       /*
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog view = builder.create();
+        final View view = LayoutInflater.from(this).inflate(R.layout.ranking_app_dialog_box,null);
+        Button Exit=view.findViewById(R.id.Exit);
+        Button Feedback=view.findViewById(R.id.feedback);
+        Button Cancel=view.findViewById(R.id.Cancel);
+*/
+        ImageButton adButton0=view.findViewById(R.id.ad_button0);
+        adButton0.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.tulip.routefinder")));}});
+        ImageButton adButton1=view.findViewById(R.id.ad_button1);
+        adButton1.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.voiceassistant")));}});
+        ImageButton adButton2=view.findViewById(R.id.ad_button2);
+        adButton2.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.navigation")));}});
+        ImageButton adButton3=view.findViewById(R.id.ad_button3);
+        adButton3.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.combo.tourtheworld")));}});
+        ImageButton adButton4=view.findViewById(R.id.ad_button4);
+        adButton4.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sparrow.quiztrivia")));}});
+        ImageButton adButton5=view.findViewById(R.id.ad_button5);
+        adButton5.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.appsstandard.unitconverter")));}});
+        ImageButton adButton6=view.findViewById(R.id.ad_button6);
+        adButton6.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sparrow.fun")));}});
+
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    finishAffinity();
+                }
+                MainActivity.this.finish();
+                System.exit(0);
+            }
+        });
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.cancel();
+            }
+        });        Feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=" + MainActivity.this.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" +
+                                    MainActivity.this.getPackageName())));
+                }
+            }
+        });
+/*
+        builder.setView(view);
+        view.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+*/
+        view.show();
+
+    }
+
+    public void onPolicyClicked(View view) { policyDialog();
+    }
+
 }
